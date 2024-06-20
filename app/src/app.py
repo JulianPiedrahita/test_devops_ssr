32bb369cdfa322886ef0660b11eb8fb7971df13d
JulianPiedrahita <65320911+JulianPiedrahita@users.noreply.github.com>
2024-06-20 09:52:16 -0500

    Update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def environment_develop():
-    return 'Hola te encuentras en el ambiente de desarrollo!'
+    return 'Hola te encuentras en el ambiente de pruebas!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=82)
